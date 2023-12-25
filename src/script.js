a1643910457b5443dacf9bcca07597dae93d0539
Ameer <ameerapex496@gmail.com>
2023-12-25 21:06:08 +0100

    selected elements needed for the code

---
+++ src/script.js
@@ -0,0 +1,15 @@
+// selected element
+const againBtn = document.getElementsByClassName("again-Btn")[0];
+const checkBtn = document.getElementsByClassName("check-Btn")[0];
+const numberInput = document.getElementsByClassName("number-Input")[0];
+const TitleText = document.getElementsByClassName("Title-Text")[0];
+const startGussingText = document.getElementsByClassName("score-1-Text")[0];
+const mainScore = document.getElementsByClassName("main-Score")[0];
+const highScore = document.getElementsByClassName("high-Score")[0];
+const decorationText = document.getElementsByClassName("decoration-Text")[0];
+
+// gelobal varibales
+
+// functions
+
+// event listner
